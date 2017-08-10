843940326e71e60cc3015e5ae22cb7cd5dda8e82
brookssj <sjbrooks@mit.edu>
2017-08-10 12:25:54 -0400

    Resolves bugs in pixel_to_xyz

--- src/pixel_to_xyz.cpp
+++ src/pixel_to_xyz.cpp
@@ -21,10 +21,13 @@ Code include methods derived from:
 */
 
 //--Declarations--//
-sensor_msgs::PointCloud2 pCloud;
+sensor_msgs::PointCloud2 pCloud_global;
 ros::Publisher point_pub;
 cv::Mat cameraMatrix(3,3,cv::DataType<double>::type);
-cv::Mat distCoeffs(1,5,cv::DataType<double>::type);
+cv::Mat distCoeffs(5,1,cv::DataType<double>::type);
+
+std::string robot_frame_global = "camera_link";
+std::string camera_frame_global = "camera_link";
 
 void transformCameraToRobot(geometry_msgs::PointStamped &camera_point,
   geometry_msgs::PointStamped &arm_link_point);
@@ -44,6 +47,8 @@ void pixel_cb(geometry_msgs::Point pixel_point);
 
 void camera_info_cb(const sensor_msgs::CameraInfo input);
 
+const sensor_msgs::PointCloud2 getPcloud() {return pCloud_global;}
+
 int main(int argc, char** argv)
 {
   // Initialize ROS
@@ -62,6 +67,10 @@ int main(int argc, char** argv)
   ros::Subscriber pixel_sub = nh.subscribe (
     "/rgb_seg/block_location", 1, pixel_cb);
 
+  // Create a ROS subscriber for the camera calibration info
+  ros::Subscriber camera_info_sub = nh.subscribe (
+    "/camera/rgb/camera_info", 1, camera_info_cb);
+
   // Spin
   ros::spin();
 }
@@ -72,13 +81,13 @@ void transformCameraToRobot(geometry_msgs::PointStamped &camera_point,
 
   // listen for transform
   tf::TransformListener listener;
-  listener.waitForTransform("/camera_link", "/arm_link_0",
+  listener.waitForTransform(camera_frame_global, robot_frame_global,
     ros::Time(0), ros::Duration(10.0));
 
   // transform camera_point to arm_link_point
   try
   {
-    listener.transformPoint("arm_link_0", camera_point, arm_link_point);
+    listener.transformPoint(robot_frame_global, camera_point, arm_link_point);
   }
   catch(tf::TransformException& ex){
     ROS_ERROR("Received an exception trying to transform a point : %s",
@@ -100,7 +109,8 @@ void pixelTo3DPointWithPartialCloud(const sensor_msgs::PointCloud2 pCloud,
   int temp_u[] = {100, 300, 500, 500, 300, 100, 100, 500, 300};
   int temp_v[] = {100, 200, 300, 100, 100, 300, 200, 200, 300};
   int idx = 0;
-  while ( (points_added < 4) && (idx < 9) ){
+  while ( (idx < 9) ) //(points_added < 4) &&
+  {
     if ( pixelToCamera3DPoint(pCloud,
       temp_u[idx], temp_v[idx], temp_camera_point) ){
       imagePoints.push_back(cv::Point2f(
@@ -113,6 +123,8 @@ void pixelTo3DPointWithPartialCloud(const sensor_msgs::PointCloud2 pCloud,
     }
     idx += 1;
   }
+
+ROS_WARN("Points added %d", points_added);
 
   // read camera parameters
   cv::Mat rvec(1,3,cv::DataType<double>::type);
@@ -131,18 +143,20 @@ void pixelTo3DPointWithPartialCloud(const sensor_msgs::PointCloud2 pCloud,
   double s;
   tempMat = rotationMatrix.inv() * cameraMatrix.inv() * uvPoint;
   tempMat2 = rotationMatrix.inv() * tvec;
-  s = 0. + tempMat2.at<double>(2,0); //0. represents the height
+  s = 0.2 + tempMat2.at<double>(2,0); //0. represents the height
   s /= tempMat.at<double>(2,0);
+
+ROS_WARN("Projection variable %.4f", s);
 
   cv::Mat temp_point = rotationMatrix.inv() * (s * cameraMatrix.inv() * uvPoint - tvec);
   std::cout << "P = " << rotationMatrix.inv() * (s * cameraMatrix.inv() * uvPoint - tvec) << std::endl;
 
   // save the point in robot frame
-  arm_link_point.header.frame_id = "arm_link_0";
+  arm_link_point.header.frame_id = robot_frame_global;
   arm_link_point.header.stamp = ros::Time();
-  arm_link_point.point.x = float(tempMat2.at<double>(0,0));
-  arm_link_point.point.x = float(tempMat2.at<double>(1,0));
-  arm_link_point.point.x = float(tempMat2.at<double>(2,0));
+  arm_link_point.point.x = float(temp_point.at<double>(0,0));
+  arm_link_point.point.y = float(temp_point.at<double>(1,0));
+  arm_link_point.point.z = float(temp_point.at<double>(2,0));
 
   return;
 }
@@ -154,6 +168,7 @@ bool pixelToCamera3DPoint(const sensor_msgs::PointCloud2 pCloud,
   int height =  pCloud.height;
 
   int arrayPosition = v*pCloud.row_step + u*pCloud.point_step;
+// ROS_WARN("pixel and array position %d, %d, %d", u, v, arrayPosition);
 
   // compute position in array where x,y,z data start
   int arrayPosX = arrayPosition + pCloud.fields[0].offset;
@@ -171,7 +186,7 @@ bool pixelToCamera3DPoint(const sensor_msgs::PointCloud2 pCloud,
   }
   else{
     // save information from depth cloud to point stamped
-    camera_point.header.frame_id = "camera_link";
+    camera_point.header.frame_id = camera_frame_global;
     camera_point.header.stamp = ros::Time();
     camera_point.point.x = X;
     camera_point.point.y = Y;
@@ -186,32 +201,39 @@ void pixelTo3DPoint(const sensor_msgs::PointCloud2 pCloud,
   int height =  pCloud.height;
 
   int arrayPosition = v*pCloud.row_step + u*pCloud.point_step;
+if (pCloud.data.size() == 0) return;
+ROS_WARN("pixel and array position %d, %d, %d", u, v, arrayPosition);
+std::cout << "size of data: " << pCloud.data.size() << " , " << sizeof(pCloud.data) << std::endl;
 
   // compute position in array where x,y,z data start
   int arrayPosX = arrayPosition + pCloud.fields[0].offset;
   int arrayPosY = arrayPosition + pCloud.fields[1].offset;
   int arrayPosZ = arrayPosition + pCloud.fields[2].offset;
 
+// ROS_WARN("array positions %d, %d, %d", arrayPosX, arrayPosY, arrayPosZ);
   // copy point cloud values at these locations
   float X = 0.0, Y = 0.0, Z = 0.0;
   memcpy(&X, &pCloud.data[arrayPosX], sizeof(float));
   memcpy(&Y, &pCloud.data[arrayPosY], sizeof(float));
   memcpy(&Z, &pCloud.data[arrayPosZ], sizeof(float));
 
-  if ( isnan(X) ){
+// std::cout << "x: " << X << " y: " << Y << " z: " << Z << std::endl;
+// ROS_WARN("coordinates %.3f, %.3f, %.3f", X, Y, Z);
+
+  if ( isnan(float(X)) ){
     ROS_WARN("Corresponding point in depth cloud missing!");
     // need to co
     pixelTo3DPointWithPartialCloud(pCloud, u, v, arm_link_point);
   }
   else{
+ROS_WARN("Point available");
     // save information from depth cloud to point stamped
     geometry_msgs::PointStamped camera_point;
-    camera_point.header.frame_id = "camera_link";
+    camera_point.header.frame_id = camera_frame_global;
     camera_point.header.stamp = ros::Time();
-    camera_point.point.x = X;
-    camera_point.point.y = Y;
-    camera_point.point.z = Z;
-
+    camera_point.point.x = float(X);
+    camera_point.point.y = float(Y);
+    camera_point.point.z = float(Z);
     // transform point to arm_link_0
     transformCameraToRobot(camera_point, arm_link_point);
   }
@@ -222,7 +244,7 @@ void pixelTo3DPoint(const sensor_msgs::PointCloud2 pCloud,
 
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 {
-  pCloud = *input;
+  pCloud_global = *input;
 }
 
 void pixel_cb(geometry_msgs::Point pixel_point)
@@ -231,8 +253,11 @@ void pixel_cb(geometry_msgs::Point pixel_point)
   int u = pixel_point.x;
   int v = pixel_point.y;
 
+// ROS_WARN("Pixel points %d, %d", u, v);
+
   // Call method to convert pixel values to xyz in base link frame
   geometry_msgs::PointStamped pixelXYZ;
+  sensor_msgs::PointCloud2 pCloud = getPcloud();
   pixelTo3DPoint(pCloud, u, v, pixelXYZ);
 }
 
@@ -246,8 +271,11 @@ void camera_info_cb(const sensor_msgs::CameraInfo input){
   }
 
   for (int idx = 0; idx < 5; idx++){
-    distCoeffs.at<double>(idx, 0) = input.K[idx];
+    distCoeffs.at<double>(idx, 0) = input.D[idx];
   }
+
+// std::cout << " Camera matrix : " << cameraMatrix << std::endl;
+// std::cout << " Dist coeffs : " << distCoeffs << std::endl;
 
   return;
 }
